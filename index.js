3cd1aac97c30d8906f0ad72ba5edb50918cb9202
AdrikLarreategui <adrian15186@hotmail.com>
2024-01-17 14:30:50 +0100

    commit usuario creado a falta campos validados e info usuario conectado

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { dbConnection } = require('./src/config/config')
 
 dbConnection()
 app.use(express, json())
-app.use('users', require('./'))
-app.use('posts', require('./'))
-app.use('comments', require('./'))
+app.use('users', require('./src/routes/users'))
+// app.use('posts', require('./'))
+// app.use('comments', require('./'))
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
